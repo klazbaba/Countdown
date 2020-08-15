056f354de6d1658cfd3b86fe53e0d7ab6e4b9a38
klazbaba <giwaklaz@gmail.com>
2020-08-15 11:16:55 +0100

    now getting time entered by user

--- app/src/main/java/com/example/countdown/MainActivity.java
+++ app/src/main/java/com/example/countdown/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
 public class MainActivity extends AppCompatActivity {
+    int totalTimeInSeconds = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +23,27 @@ public class MainActivity extends AppCompatActivity {
 
     public void startTimer(String seconds, String minutes, String hours) {
         TextView time = findViewById(R.id.time);
-        time.setText(hours + minutes + seconds);
+        totalTimeInSeconds = convertTimeToSeconds(seconds, minutes, hours);
+        String timeToDisplay = String.format("%02d:%02d:%02d", getHours(totalTimeInSeconds), getMinutes(totalTimeInSeconds), totalTimeInSeconds);
+        time.setText(timeToDisplay);
+    }
+
+    private int getHours(int time) {
+        totalTimeInSeconds = time % 3600;
+        return time / 3600;
+    }
+
+    private int getMinutes(int time) {
+        totalTimeInSeconds = time % 60;
+        return time / 60;
+    }
+
+    private int convertTimeToSeconds(String seconds, String minutes, String hours) {
+        int hoursInteger = Integer.parseInt(hours);
+        int minutesInteger = Integer.parseInt(minutes);
+        int secondsInteger = Integer.parseInt(seconds);
+
+        return hoursInteger * 3600 + minutesInteger * 60 + secondsInteger;
+
     }
 }
